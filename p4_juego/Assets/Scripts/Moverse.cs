a339ca3bc2c4ea88d4264a4c3c1a1723cd06f0fc
FuerzaOperativa <fernando.parras@live.u-tad.com>
2020-03-25 20:22:39 +0100

    Added new Scene, added 1 Movement Script, Movement Script Implemented to the character at the new one .Scene named Test_Fernando

---
+++ p4_juego/Assets/Scripts/Moverse.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moverse : MonoBehaviour
+{
+    CharacterController m_controller;
+    public float speed = 4;
+    public float aceleracion = 1;
+    public float gravedad = -9.81f;
+    public float salto = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+
+        float InputX = Input.GetAxis("Horizontal");
+        float InputY = Input.GetAxis("Vertical");
+        Vector3 Moving = new Vector3(InputX * speed * aceleracion, salto, InputY * speed * aceleracion);
+
+        m_controller.Move(Moving * Time.deltaTime);
+        Up();
+        Gravity();
+    }
+    void Run()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (aceleracion < 3)
+            {
+                aceleracion++;
+            }
+        }
+    }
+    void Up()
+    {
+        if (m_controller.isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            salto = 12;
+        }
+    }
+    void Gravity()
+    {
+        salto += gravedad * Time.deltaTime;
+    }
+}
